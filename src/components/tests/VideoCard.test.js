f23e5c342fcd5cac4b7b174a47a03af3de1659a0
kangmuni <kangmoonee@gmail.com>
2024-03-18 12:39:16 +0900

    비디오 카드 컴포넌트 테스트 리팩토링

--- src/components/tests/VideoCard.test.js
+++ src/components/tests/VideoCard.test.js
@@ -1,33 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { MemoryRouter, Route, Routes, useLocation } from 'react-router-dom';
+import { Route, useLocation } from 'react-router-dom';
 import { formatAgo } from '../../util/date';
 import VideoCard from '../VideoCard';
+import { withRouter } from '../../tests/utils';
+import { fakeVideo as video } from '../../tests/videos';
 
 describe('VideoCard', () => {
-  const video = {
-    id: 1,
-    snippet: {
-      title: 'title',
-      channelId: '1',
-      channelTitle: 'channelTitle',
-      publishedAt: new Date(),
-      thumbnails: {
-        medium: {
-          url: 'http://image/',
-        },
-      },
-    },
-  };
   const { title, channelTitle, publishedAt, thumbnails } = video.snippet;
 
   // 정적 테스트
   it('renders video item', () => {
     render(
-      // 라우터 사용시에는 MemoryRouter 꼭 사용해주기
-      <MemoryRouter>
-        <VideoCard video={video} />
-      </MemoryRouter>
+      withRouter(<Route path="/" element={<VideoCard video={video} />} />)
     );
 
     // 비디오가 보여졌을때 이런것들이 보여져야한다. (컴포넌트 단위로 테스트 작성)
@@ -48,15 +33,15 @@ describe('VideoCard', () => {
 
     // 원하는경로로 이동하는지 확인
     render(
-      <MemoryRouter initialEntries={['/']}>
-        <Routes>
+      withRouter(
+        <>
           <Route path="/" element={<VideoCard video={video} />} />
           <Route
             path={`/videos/watch/${video.id}`}
             element={<LocationStateDisplay />}
           />
-        </Routes>
-      </MemoryRouter>
+        </>
+      )
     );
 
     // 비디오 카드에 원하는 state가 있는지 확인
