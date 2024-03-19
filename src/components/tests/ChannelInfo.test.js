83911388edc03f817d5fd13df2df2096a13e7c8c
kangmuni <kangmoonee@gmail.com>
2024-03-19 17:34:41 +0900

    채널 컴포넌트 테스트 작성

--- src/components/tests/ChannelInfo.test.js
+++ src/components/tests/ChannelInfo.test.js
@@ -14,8 +14,32 @@ describe('ChannelInfo', () => {
   it('renders correctly', async () => {
     // 호출되면 url을 리턴
     fakeYoutube.channelImageURL.mockImplementation(() => 'url');
+    // url 받아오는 부분이 비동기이기때문에 그냥 스냅샷을 하면 이름만 나온다. url이 있을때까지 기다려야하므로 먼저 render를 해야한다.
+    const { asFragment } = renderChannelInfo();
 
-    render(
+    await waitFor(() => screen.getByRole('img'));
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('renders without URL', () => {
+    fakeYoutube.channelImageURL.mockImplementation(() => {
+      throw new Error('error');
+    });
+    renderChannelInfo();
+
+    expect(screen.queryByRole('img')).toBeNull();
+  });
+
+  it('renders with URL', async () => {
+    fakeYoutube.channelImageURL.mockImplementation(() => 'url');
+
+    renderChannelInfo();
+
+    await waitFor(() => expect(screen.getByRole('img')).toBeInTheDocument());
+  });
+
+  function renderChannelInfo() {
+    return render(
       withAllContexts(
         withRouter(
           <Route path="/" element={<ChannelInfo id="id" name="channel" />} />
@@ -23,8 +47,5 @@ describe('ChannelInfo', () => {
         fakeYoutube
       )
     );
-
-    // 쿼리는 무조건 비동기니까 await
-    await waitFor(() => screen.getByText('channel'));
-  });
+  }
 });
