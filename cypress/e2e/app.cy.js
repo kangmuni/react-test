46ff5c7c79803e577201a5669a3010cb4754cd44
kangmuni <kangmoonee@gmail.com>
2024-03-20 16:39:12 +0900

    fixtures mock 데이터 만들기, 안정적인 테스트 작성

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -3,10 +3,21 @@ import '@testing-library/cypress/add-commands';
 
 describe('Youtube App', () => {
   beforeEach(() => {
+    cy.intercept('GET', /(mostPopular)/g, {
+      fixture: 'popular.json',
+    });
+    cy.intercept('GET', /(search)/g, {
+      fixture: 'search.json',
+    });
+    cy.viewport(1200, 800);
     cy.visit('/');
   });
 
   it('renders', () => {
     cy.findByText('Youtube').should('exist');
+  });
+
+  it('shows popluar video first', () => {
+    cy.findByText('Popular Video').should('exist');
   });
 });
