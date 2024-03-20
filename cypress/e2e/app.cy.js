125fedb6fa6a4ec900960b118de4c3cfc49036ff
kangmuni <kangmoonee@gmail.com>
2024-03-20 16:41:29 +0900

    나머지 기능들 테스트 작성

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -20,4 +20,17 @@ describe('Youtube App', () => {
   it('shows popluar video first', () => {
     cy.findByText('Popular Video').should('exist');
   });
+
+  it('searches by keyword', () => {
+    cy.findByPlaceholderText('Search...').type('bts');
+    cy.findByRole('button').click();
+    cy.findByText('Search Result1').should('exist');
+  });
+
+  it('goes to detail page', () => {
+    cy.findAllByRole('listitem').first().click();
+    cy.findByTitle('Popular Video').should('exist');
+    cy.findByText('Popular Video').should('exist');
+    cy.findByText('Search Result1').should('exist');
+  });
 });
